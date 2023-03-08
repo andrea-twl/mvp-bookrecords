cad12e6c2c523e2a4dd9c8e32278ac2f0570af6a
andrea-twl <tan.wanling.andrea@gmail.com>
2023-03-08 22:18:14 +0800

    update: toggle behaviour

--- frontend/src/components/molecules/BookItem.jsx
+++ frontend/src/components/molecules/BookItem.jsx
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookToggleIcon from "../../assets/icons/BookToggleIcon";
 import BookItemHeading from "../atoms/BookItemHeading";
 import BookItemSubHeading from "../atoms/BookItemSubHeading";
 import Customer from "../atoms/Customer";
 import styles from "./BookItem.module.scss";
 
-const BookItem = ({ book, id }) => {
+const BookItem = ({ book, id, activeItem, setActiveItem }) => {
   const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    if (activeItem != id) {
+      setToggle(false);
+    }
+  }, [activeItem]);
+
+  const handleClick = () => {
+    if (toggle) {
+      setActiveItem(null);
+      setToggle(false);
+    } else {
+      setActiveItem(id);
+      setToggle(true);
+    }
+  };
+
   return (
     book && (
       <div className={`${styles["book__item"]}`} id={`book-item-${id}`}>
@@ -17,10 +34,7 @@ const BookItem = ({ book, id }) => {
           <div className={styles["book__name"]}>
             <BookItemHeading text={book.name} />
           </div>
-          <div
-            className={styles["book-toggle"]}
-            onClick={() => setToggle(!toggle)}
-          >
+          <div className={styles["book-toggle"]} onClick={handleClick}>
             <BookToggleIcon />
           </div>
           <div className={styles["book__author"]}>
--- frontend/src/components/organisms/BookList.jsx
+++ frontend/src/components/organisms/BookList.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import BookToggleIcon from "../../assets/icons/BookToggleIcon";
 import BookItemHeading from "../atoms/BookItemHeading";
 import BookItemSubHeading from "../atoms/BookItemSubHeading";
 import BookItem from "../molecules/BookItem";
 import styles from "./BookList.module.scss";
 
-const BookList = ({ data, id, name, author }) => {
+const BookList = ({ data }) => {
+  const [activeItem, setActiveItem] = useState(null);
   return (
-    <div className={`${styles["book__list"]}`} id={`book-item-${id}`}>
+    <div className={`${styles["book__list"]}`} id={"container"}>
       {data.map((book, id) => (
-        <BookItem book={book} id={id + 1} />
+        <BookItem
+          book={book}
+          id={id + 1}
+          activeItem={activeItem}
+          setActiveItem={setActiveItem}
+        />
       ))}
     </div>
   );
