557166a0125712c376fc193e989bb11a9769ad36
andrea-twl <tan.wanling.andrea@gmail.com>
2023-03-09 15:01:36 +0800

    add: tests

---
+++ bookrecords/src/main/resources/test_data/emptyData.sql
@@ -0,0 +1,5 @@
+DELETE FROM book_rents;
+DELETE FROM author_books;
+DELETE FROM books;
+DELETE FROM authors;
+DELETE FROM people;
--- bookrecords/src/test/java/com/example/bookrecords/repository/BooksRepositoryTest.java
+++ bookrecords/src/test/java/com/example/bookrecords/repository/BooksRepositoryTest.java
@@ -2,16 +2,22 @@ package com.example.bookrecords.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// @DataJpaTest
-@SpringBootTest
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+// @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@DataJpaTest
+//@SpringBootTest
 class BooksRepositoryTest {
     @Autowired
     private BooksRepository repo;
@@ -34,9 +40,11 @@ class BooksRepositoryTest {
     }
 
     private void compareNestedList(List<List<Object>> expected, List<List<Object>> result) {
+        assertEquals(expected.size(), result.size());
         for (int i = 0; i < expected.size() && i < result.size(); i++) {
             List<Object> nestedResult = result.get(i);
             List<Object> nestedExpected = expected.get(i);
+            assertEquals(nestedExpected.size(), nestedResult.size());
             for (int j = 0; j < nestedExpected.size() && j < nestedResult.size(); j++) {
                 assertTrue(nestedExpected.get(j).equals(nestedResult.get(j)));
             }
@@ -52,6 +60,30 @@ class BooksRepositoryTest {
                 getObjLs("Mort",2, "Terry Pratchet"),
                 getObjLs("Reaper Man",4, "Terry Pratchet"));
 
+        compareNestedList(expected, result);
+    }
+
+    @Test
+    @Sql(scripts={"classpath:test_data/emptyData.sql"})
+    public void getTop3ReadBook_NoData_ReturnCorrectList() {
+        // When
+        List<List<Object>> result = repo.getTop3ReadBook();
+
+        // Then
+        setExpected();
+        compareNestedList(expected, result);
+    }
+
+    @Test
+    @Sql(scripts={"classpath:test_data/oneValidBook.sql"})
+    public void getTop3ReadBook_OneBookTwoAuthors_ReturnCorrectList() {
+        // When
+        List<List<Object>> result = repo.getTop3ReadBook();
+
+        // Then
+        setExpected(getObjLs("Good Omens",3, "Terry Pratchet"),
+                getObjLs("Mort",2, "Terry Pratchet"),
+                getObjLs("Reaper Man",4, "Terry Pratchet"));
         compareNestedList(expected, result);
     }
 
