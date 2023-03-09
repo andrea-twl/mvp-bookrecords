844c1d39e7214c700aa2edb4b9c18937d6ef9ed8
andrea-twl <77443618+andrea-twl@users.noreply.github.com>
2023-03-09 16:38:19 +0800

    Delete sql directory

--- sql/main.sql
+++
@@ -1,4 +0,0 @@
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-\i 'C:/Users/Andrea/Desktop/andrea/work/Toppan/bookrecords-project/sql/create_tables2.sql'
-\i 'C:/Users/Andrea/Desktop/andrea/work/Toppan/bookrecords-project/sql/input_mock_data2.sql'
